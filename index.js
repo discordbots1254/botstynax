dc08ed3c12743e2b5a8c0f9ac69388e608d9a4d9
discordbots1254 <32745296+discordbots1254@users.noreply.github.com>
2017-10-20 18:31:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const Discord = require("discord.js");
 
-const TOKEN = "MzY4NjY4MTg2ODM4Njk1OTM3.DMNUiA.baB8gXXdmE8tCRQwWIOtmS4THX4";
+const TOKEN = "MzcwOTY0NTQxOTE3NDk1Mjk2.DMuyXg.4MQHLKf-avVwkBYtf2uHD8heevA";
 
 var bot = new Discord.Client();
 const PREFIX = ">>";
-
-
 
 
 bot.on("message",function(message){
@@ -18,31 +16,36 @@ bot.on("message",function(message){
     
     var args = message.content.substring(PREFIX.length).split(" ");
     
+
+
     switch (args[0].toLowerCase()){
     
-      
         case "buy":
+        message.channel.send({embed: {
+            color: 3447003,
+            title: "Buy vero",
+            url: "",
+            author: {
+                name: "Vero Bot",
+                icon_url: "https://cdn.discordapp.com/avatars/370964541917495296/3457e4030ed8da03fb959a825b8f6831.png?size=128"
+              },
+            description: "For buy vero you need :",
+            fields: [{
+                name: "Gift Cards",
+                value: "Minimum 5$ (PSN , XBOX , STEAM , ROBLOX , ROBUX (260 if you have a group for send robux) OR AMAZON."
+              },
+            
+             
+            ],
+            timestamp: new Date(),
+            footer: {
+              text: "©Vero Bot 2017"
+            }
+          }
+        });
+        
 
-    message.channel.send({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "Buy Vero",
-  
-       
-    description: "You can buy Vero with",
-    fields: [{
-        name: "Gift Card ",
-        value: "Roblox Card , Psn card , Steam card , or Robux (260) dm the owner for buy it! || NO PAYPAL||"
-      },
-     
-    ],
-});
-      
-    
-      break;
+        break;
     
     
     
@@ -56,4 +59,4 @@ bot.on("message",function(message){
     
     });
     
-    bot.login(process.env.BOT_TOKEN);
+    bot.login(TOKEN);
