e8a9b3896462d477c72edbc9defd7182cae028dc
discordbots1254 <32745296+discordbots1254@users.noreply.github.com>
2017-11-23 10:57:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,129 +1,52 @@
 const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-const TOKEN = "MzcwOTY0NTQxOTE3NDk1Mjk2.DM9fDg.EWT_vIujfVbqbZPGBh7Y39lNo4E";
-
-var bot = new Discord.Client();
-const PREFIX = ">>";
-
-
-bot.on("message",function(message){
-    
-    
-    
-    
-    if (message.author.equals(bot.user)) return;
-    if (!message.content.startsWith(PREFIX)) return;
-    
-    var args = message.content.substring(PREFIX.length).split(" ");
-    
-
-
-    switch (args[0].toLowerCase()){
-    
-        case "buy":
-        message.channel.send({embed: {
-            color: 3447003,
-            title: "Buy vero",
-            url: "",
-            author: {
-                name: "Vero Bot",
-                icon_url: "https://cdn.discordapp.com/avatars/370964541917495296/3457e4030ed8da03fb959a825b8f6831.png?size=128"
-              },
-            description: "For buy vero you need :",
-            fields: [{
-                name: "Gift Cards",
-                value: "Minimum 5$ (PSN , XBOX , STEAM , ROBLOX , ROBUX (260 if you have a group for send robux) OR AMAZON."
-              },
-            
-             
-            ],
-            timestamp: new Date(),
-            footer: {
-              text: "©Vero Bot 2017"
-            }
-          }
-        });
-        
-
-        break;
-            
-            case "website":
-        message.channel.send({embed: {
-            color: 3447003,
-            title: "Website of Vero",
-            url: "",
-            author: {
-                name: "Vero Bot",
-                icon_url: "https://cdn.discordapp.com/avatars/370964541917495296/3457e4030ed8da03fb959a825b8f6831.png?size=128"
-              },
-            description: "Vero Website",
-            fields: [{
-                name: "Link :",
-                value: "http://veroscripthub.cf/"
-              },
-            
-             
-            ],
-            timestamp: new Date(),
-            footer: {
-              text: "©Vero Bot 2017"
-            }
-          }
-        });
-        
-
-        break;
-    
-        case "help":
-        message.channel.send({embed: {
-            color: 3447003,
-            title: "Help 4 Vero",
-            url: "",
-            author: {
-                name: "Vero Bot",
-                icon_url: "https://cdn.discordapp.com/avatars/370964541917495296/3457e4030ed8da03fb959a825b8f6831.png?size=128"
-              },
-            description: "Commands :",
-            
-
-              fields: [{
-                  name: ">>buy / >>website",
-                  value: "Get infos for buy / get link of website"
-              },
-                    
-            
-           
-            
-             
-            ],
-            timestamp: new Date(),
-            footer: {
-              text: "©Vero Bot 2017"
-            }
-          }
-        });
-        
-
-        break;
-            
-          case "codesecretfr":
-       bot.user.setGame('sales on (dm  the owner or type ">>buy")');
-
-        break;
-
-    
-   
-        
-    
-    
-        default:
-        message.channel.sendMessage("ERROR COMMAND!");
-    
+client.on('message', msg => {
+  if (msg.content === ';fire me') {
+    msg.reply("HOLY SHIT YOUR BODY IS DIE")
+  }
+});
+client.on('message', msg => {
+    if (msg.content === ';sp me') {
+      msg.reply("Sparkles added")
     }
-    
-    });
-    
-    
+  });
 
-    
-    bot.login(process.env.BOT_TOKEN);
+  client.on('message', msg => {
+    if (msg.content === ';admin me') {
+      msg.reply("LOL YOU CAN'T ADMIN YOU")
+    }
+  });
+
+  client.on('message', msg => {
+      if (msg.content === 'ping') {
+        msg.reply("``"+new Date().getTime() - msg.createdTimestamp + "``"  + " ms");
+
+
+
+      }
+
+
+
+  });
+ 
+  client.on(`message`, message => {
+    if (message.content === "/download") {
+      let modRole = message.guild.roles.find("name", "Buyers");
+      if(message.member.roles.has(modRole.id)) {
+        message.channel.sendMessage(`YAY! Your an Buyers check your DMs.`)
+        message.author.sendMessage("Thanks for purchasing :")
+        message.author.sendMessage("Website : http://bridge-script-hub-official.cf ")
+        message.author.sendMessage("Download : http://bridge-script-hub-official.cf/Bridges%20files.zip ")
+        message.author.sendMessage("Forum : http://bridge-script-hub-official.cf/forum/ ")
+        message.author.sendMessage("Whitelist website : http://bridge-script-hub-official.cf/ClientPage/ `DM THE OWNER FOR GET YOUR USERNAME AND PASSWORD`")
+      } else {
+        return message.reply("Your not an Buyers :(")
+      }
+    }
+  });
+
+client.login('process.env.BOT_TOKEN');
